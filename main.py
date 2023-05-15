104fb57341c69ceb2ec9695ff2fd2b209e69287a
KrzysztofNer <krzysner@gmail.com>
2023-05-15 21:59:46 +0200

    more advanced version with example in test.py

--- main.py
+++ main.py
@@ -1,21 +1,54 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {"tim" : {"age":19, "gender": "male"},
-         "bill":{"age":40, "gender": "male"}}
+# Automaticlly parse through the request and fits the kind of gidline
+# That we're about define(usefull information: name video, its amount likes and views
+video_put_args = reqparse.RequestParser()
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
+video_put_args.add_argument(
+    "name",
+    type=str,
+    help="Name of th video is required",
+    # what we should display if the user doesn't provide the argument. It is kind of error message
+    required=True
+)
+video_put_args.add_argument("views", type=int, help="Amount views of th video", required=True)
+video_put_args.add_argument("likes", type=int, help="Amount likes of th video", required=True)
 
-    # def post(self):
-    #     return {"data": "Posted"}
+videos = {}
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+
+def abort_if_video_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Could not find video...")
+
+
+def abort_if_video_id_exist(video_id):
+    if video_id in videos:
+        abort(409, message="Video already exist with that ID...")
+
+
+class Video(Resource):
+    def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        return videos[video_id]
+
+    def put(self, video_id):
+        abort_if_video_id_exist(video_id)
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201  # 201 is the status code for created
+
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        videos.pop(video_id)  # we can also use del videos[video_id]
+        return '', 204  # 204 is the status code for deleted
+
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- test.py
+++ test.py
@@ -2,6 +2,24 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
+data = [{"likes": 55, "name": "tede", "views": 321}, {"likes": 864, "name": "edzio", "views": 33413},
+        {"likes": 553, "name": "peja", "views": 33653}, {"likes": 54645, "name": "50cent", "views": 5326},
+        {"likes": 255, "name": "50cent", "views": 31431}, {"likes": 4636, "name": "eminem", "views": 875453}]
 
-response = requests.get(BASE + "helloworld/bill")
+for i in range(len(data)):
+    response = requests.put(f"{BASE}video/{i}", data[i])
+    print(response.json())
+# response = requests.put(BASE + "video/3", {"likes":55, "name":"50cent", "views":333})
+# print(response.json())
+input()
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.delete(BASE + "video/2")
+print(response)
+input()
+response = requests.get(BASE + "video/4")
+print(response.json())
+input()
+response = requests.get(BASE + "video/2")
 print(response.json())
